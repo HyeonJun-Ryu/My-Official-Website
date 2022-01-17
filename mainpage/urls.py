2bc00003e0708ce6aa8d9c41318b0fe91167cd66
HyeonJun Ryu <hjryu1816@gmail.com>
2022-01-17 17:28:56 +0900

    Day6(22.1.17.) 웹사이트 레이아웃 전체 변경 및 링크 작업 완료
    세부 기능 구현 필요.

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('tables', views.tables, name='tables'),
+    path('forms', views.forms, name='forms'),
+    path('register', views.register, name='register'),
+    path('charts', views.charts, name='charts'),
     path('post', views.post, name='post'),
     path('post/<int:boardid>/', views.detail, name='detail'),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -2,14 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.utils import timezone
+import pymongo
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
-db = client.MP # MP 이름의 DB에 접근
-
+boarddb = client.MP # MP 이름의 DB에 접근
+knudb = client.MP
 
 def index(request):
     boards = {'boards': db.board.find()}
-    return render(request, 'mainpage/index.html', boards)
+    return render(request, 'mainpage/index2.html', boards)
+
+def login(request):
+    return render(request, 'mainpage/login.html')
+
+def tables(request):
+    boards = {'boards': knudb.knuboard.find().sort("boardid", pymongo.DESCENDING)}
+    return render(request, 'mainpage/tables.html', boards)
+
+def forms(request):
+    return render(request, 'mainpage/forms.html')
+
+def register(request):
+    return render(request, 'mainpage/register.html')
+
+def charts(request):
+    return render(request, 'mainpage/charts.html')
 
 def post(request):
     if request.method == "POST":
